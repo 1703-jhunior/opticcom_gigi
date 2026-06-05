d1baef6a2911b6ea1ca7fdf6a54ecb68d2481d7b
1703-jhunior <i2414547@continental.edu.pe>
2026-06-05 14:16:19 -0500

    Refactor vista method for absolute path check
    Updated vista method to check for view file existence using absolute path and added error message.

--- app/controladores/Controlador.php
+++ app/controladores/Controlador.php
@@ -5,10 +5,12 @@ class Controlador {
         return new $modelo();
     }
 
-    public function vista($vista, $datos = []){
-        if (file_exists('../app/vistas/' . $vista . '.php')) {
-            require_once '../app/vistas/' . $vista . '.php';
+public function vista($vista, $datos = []) {
+        // Chequear si el archivo vista existe usando ruta absoluta
+        if (file_exists(__DIR__ . '/../vistas/' . $vista . '.php')) {
+            require_once __DIR__ . '/../vistas/' . $vista . '.php';
         } else {
+            // Este es el mensaje que estás viendo ahora mismo
             die('Error Crítico: La vista no existe.');
         }
     }
