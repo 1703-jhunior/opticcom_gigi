8efb58a3db86975cefbcd51891738d428bc89a1a
1703-jhunior <i2414547@continental.edu.pe>
2026-06-05 13:58:32 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 // --- Archivo de Entrada Principal de la Aplicación ---
 
 // Cargamos el archivo de arranque (bootstrap) que prepara todo el sistema.
-require_once '../app/bootstrap.php';
+require_once __DIR__ . '/../app/bootstrap.php';
 
 // Creamos una instancia de la clase Core para que maneje la URL e inicie la aplicación.
 $iniciar = new Core();
