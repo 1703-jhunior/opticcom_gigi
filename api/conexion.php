cbcb52d81cb90a7346fd04696eed370248da98c4
1703-jhunior <i2414547@continental.edu.pe>
2026-06-19 12:49:57 -0500

    Change database host in Conexion class

--- api/conexion.php
+++ api/conexion.php
@@ -6,20 +6,17 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Content-Type: application/json; charset=UTF-8");
-
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
     exit();
 }
 // =======================================================================
-
 class Conexion {
-    private $host = '192.168.1.128';
+    private $host = 'ns6jr3esckb9oucez229qsx1';
     private $usuario = 'mariadb'; 
     private $password = 'root1234'; 
     private $db = 'default';
     private $dbh;
-
     public function conectar(){
         $dsn = 'mysql:host=' . $this->host . ';port=3306;dbname=' . $this->db . ';charset=utf8mb4';
         $opciones = [
@@ -27,7 +24,6 @@ class Conexion {
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ];
-
         try {
             $this->dbh = new PDO($dsn, $this->usuario, $this->password, $opciones);
             return $this->dbh;
